d37f3b113938938c18793355afdf54e3d0fceefc
arepi-nemui <159742305+arepi-nemui@users.noreply.github.com>
2024-02-12 03:45:59 +0000

    make get_key function for easier debugging
    ghidra tries to interpret the string logic as + "\a" instead of
    decimal 7

--- rev1.c
+++ rev1.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
 #include <string.h>
 
+int get_key() {
+    return 7;
+}
+
 int main(int argc, char **argv) {
     if (argc < 2) {
         printf("%s%s%s", "usage: ", argv[0], " flag\n");
         return 2;
-    }
-    if (argc > 9999) {
-        puts("use secure secret value of the princess' age\n");
     }
 
     char *flag = ":P:<M?tZX<*Ia,kX?*MX_)kX:Xik*g<,..v";
     int i;
     int flaglen = strlen(flag);
     char output[flaglen];
-    int key = 7;
+    int key = get_key();
+
+    if (strcmp(argv[1], "hint") == 0) {
+        puts("use secure secret value of the princess' age\n");
+        return key;
+    }
 
     for (i = 0; i < flaglen; i++) {
         output[i]=flag[i]+key;
