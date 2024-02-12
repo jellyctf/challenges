13e9d897b242c510807948518149e8f50715cc98
arepi-nemui <159742305+arepi-nemui@users.noreply.github.com>
2024-02-12 03:17:29 +0000

    Create rev1.c

---
+++ rev1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char **argv) {
+    if(argc < 1) {
+        printf("%s%s%s", "usage: ", argv[0], " flag");
+        return 2;
+    }
+    if (argc > 9999) {
+        puts("use secure secret value of the princess' age\n");
+    }
+
+    char *flag = ":P:<M?tZX<*Ia,kX?*MX_)kX:Xik*g<,..v";
+    int i;
+    int flaglen = strlen(flag);
+    char output[flaglen];
+    int key = 7;
+
+    for (i = 0; i < flaglen; i++) {
+        output[i]=flag[i]+key;
+    }
+    if (strcmp(output, argv[1]) == 0) {
+        puts("Flag correct!\n");
+        return 0;
+    }
+    else {
+        puts("Flag incorrect.\n");
+        return 1;
+    }
+}
