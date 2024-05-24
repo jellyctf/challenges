ddaba4d5ee00348bc1261a66ce264ff1ec0d59cb
arepi-nemui <159742305+arepi-nemui@users.noreply.github.com>
2024-05-24 01:56:54 +0000

    add session state to factory_clicker

--- web/factory_clicker/src/main.py
+++ web/factory_clicker/src/main.py
@@ -1,26 +1,24 @@
-from flask import Flask, render_template, jsonify, request
+import secrets
+from datetime import timedelta
+from flask import Flask, render_template, jsonify, request, session
 
 app = Flask(__name__, static_url_path='/static')
+app.secret_key = secrets.token_urlsafe(32)
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=1)
 
-score = 0
-flag = ""
 
 @app.route('/')
 def index():
-    global score 
-    global flag 
-    flag = ""
-    score = 0
-    return render_template('index.html', score=0)
+    score = session.get("score", 0)
+    return render_template('index.html', score=score)
 
 @app.route('/increment', methods=['POST'])
 def increment():
-    global score
-    global flag
     increment_amount = int(request.args.get('increment_amount', 1))
-    score += increment_amount
-    if score > 500000000000: flag = "jellyCTF{keep_on_piping_jelly}"
-    return jsonify({'score': score, 'flag': flag})
+    session['score'] = session.get("score", 0) + increment_amount
+    if session['score'] > 500000000000:
+        return jsonify({'score': session['score'], 'flag': "jellyCTF{keep_on_piping_jelly}"})
+    return jsonify({'score': session['score'], 'flag': ""})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
