3109e734dbc135753e0a8de4feb8887b49ead2f8
arepi-nemui <159742305+arepi-nemui@users.noreply.github.com>
2024-05-25 22:26:14 +0000

    add passcode_lock Dockerfile, dist and solve

---
+++ crypto/passcode_lock/dist/passcode_lock.c
@@ -0,0 +1,80 @@
+// de Bruijn brute force challenge
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int runChallenge(int passcodeLength)
+{
+    int EXTRA_ALLOWANCE = passcodeLength;
+    int bitmask = (1 << passcodeLength) - 1;
+    int maxTimeout = bitmask;
+    int passCode = random() & bitmask;
+
+    printf("Debug random passcode value: %u\n", passCode);
+    printf("WARNING: System will timeout after %d entries\n", maxTimeout + EXTRA_ALLOWANCE);
+    printf("Enter %d-digit binary passcode  \n", passcodeLength);
+
+    int timeout = 0;
+    int userInput = 0;
+
+    char received;
+
+    while (timeout < maxTimeout + EXTRA_ALLOWANCE)
+    {
+        scanf(" %c", &received);
+        printf("Received: %c\n", received);
+        printf("Received equality: %d\n", received != '0');
+        printf("Debug %u\n", userInput);
+
+        userInput = bitmask & (userInput << 1 | (received != '0'));
+        if (userInput == passCode)
+        {
+            return 1;
+        }
+
+        if (timeout % passcodeLength == 4)
+        {
+            printf("Passcode incorrect. Try again!\n");
+        }
+
+        timeout++;
+    }
+
+    printf("Timeout exceeded\n");
+
+    return 0;
+}
+
+int main(int argc, char **argv) {
+    srand(time(NULL));
+
+    int PASSCODE_LENGTH = 5;
+    int NUM_PHASES = 10;
+
+    int phase;
+    int result1 = 0;
+
+    for (phase = 1; phase <= NUM_PHASES; phase++)
+    {
+        printf("\n\nStarting Phase %d...\n", phase);
+        result1 = runChallenge(PASSCODE_LENGTH);
+
+        // flush stdin
+        int c;
+        while((c = getchar()) != '\n' && c != EOF);
+
+        printf("Phase %d validation result: %d\n", phase, result1);
+
+        if (result1 == 0)
+        {
+            break;
+        }
+    }
+
+    if (phase == NUM_PHASES + 1)
+    {
+        printf("Win! Flag is: jellyCTF{redacted}\n");
+    }
+
+    return 0;
+}
---
+++ crypto/passcode_lock/solve/solve.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from pwn import *
+
+p = process("./passcode_lock")
+
+n = 5
+g = cyclic_gen("01", n=n)
+seq = g.get()
+info([seq[i:i+n] for i in range(0, len(seq), n)])
+
+for phase in range(10):
+    for i in range(len(seq)//n):
+        info(seq)
+        try:
+            out = p.sendlineafter((b"Enter 5-digit binary passcode", b"Passcode incorrect. Try again!"), seq)
+        except EOFError:
+            info(b"EOF " + out)
+        info(out)
+        if b"Phase" in out:
+            break
+p.interactive()
